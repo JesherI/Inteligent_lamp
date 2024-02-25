b34a1750f377c2574cd9e64e711eae55ab621f8e
Jesher <jesherjesher1@gmail.com>
2024-02-25 15:52:42 -0600

    01 version

---
+++ core/static/js/script-base.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const menuIcon = document.getElementById('menu-icon');
+    const nav = document.querySelector('nav');
+    const intelligentLamp = document.querySelector('#inicio .titles');
+
+    menuIcon.addEventListener('click', function () {
+        nav.classList.toggle('active');
+    });
+
+    document.body.onload = function () {
+        intelligentLamp.classList.add('animate');
+        const intelligent = document.querySelector('#inicio .intelligent');
+        const lamp = document.querySelector('#inicio .lamp');
+        intelligent.classList.add('animate');
+        lamp.classList.add('animate');
+    };
+
+    const iconImages = document.querySelectorAll('.icon-container img');
+    iconImages.forEach(icon => {
+        icon.addEventListener('mouseover', function () {
+            icon.style.transform = 'scale(1.5)';
+        });
+
+        icon.addEventListener('mouseout', function () {
+            icon.style.transform = 'scale(1)';
+        });
+    });
+
+    intelligentLamp.addEventListener('mouseover', function () {
+        intelligentLamp.style.color = '#EDD4D2'; 
+        intelligentLamp.style.transform = 'translateY(-3px)'; 
+    });
+
+    intelligentLamp.addEventListener('mouseout', function () {
+        intelligentLamp.style.color = '#fff'; 
+        intelligentLamp.style.transform = 'translateY(0)';
+    });
+});
