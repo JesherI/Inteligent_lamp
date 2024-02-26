850faa908a2fa741a90835a84faa7333e6ca669e
Jesher <jesherjesher1@gmail.com>
2024-02-25 20:09:17 -0600

    04 version

--- core/static/js/script-responsividad.js
+++ core/static/js/script-responsividad.js
@@ -1,5 +1,11 @@
-// Función para alternar la visibilidad del menú desplegable
-document.getElementById('menu-icon').addEventListener('click', function() {
-    var menu = document.querySelector('.menu');
-    menu.classList.toggle('active');
+document.getElementById("menu-icon").addEventListener("click", function() {
+    var menu = document.querySelector(".menu");
+    menu.classList.toggle("active");
+    
+    if (!menu.classList.contains("active")) {
+        menu.classList.add("slideMenuOut");
+        setTimeout(function() {
+            menu.classList.remove("slideMenuOut");
+        }, 500); // Asegurarse de eliminar la clase después de que termine la animación
+    }
 });
